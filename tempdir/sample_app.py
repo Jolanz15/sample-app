af4f1a28e570a075f81e6efa142af66d78a56d95
Jolanz15 <mj.gbriel@gmail.com>
2025-03-31 03:05:02 +0000

    Changed port from 8080 to 5050.

--- tempdir/sample_app.py
+++ tempdir/sample_app.py
@@ -7,12 +7,5 @@ def main():
     return  render_template("index.html")
 
 if __name__ == "__main__":
-    sample.run(host="0.0.0.0", port=8080)
+    sample.run(host="0.0.0.0", port=5000)
 
-
-echo "RUN pip install flask" >> tempdir/Dockerfile
-echo "COPY ./static /home/myapp/static/" >> tempdir/Dockerfile
-echo "COPY ./templates /home/myapp/templates/" >> tempdir/Dockerfile
-echo "COPY sample_app.py /home/myapp/" >> tempdir/Dockerfile
-echo "EXPOSE 8080" >> tempdir/Dockerfile
-echo "CMD python3 /home/myapp/sample_app.py" >> tempdir/Dockerfile
